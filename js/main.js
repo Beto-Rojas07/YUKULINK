8967693b555990e69775a824ab0fce76a9f08803
Alberto Rojas Mendoza <albertorojasmen@gmail.com>
2025-12-02 13:33:29 -0600

    Ajustes finales de responsividad

--- js/main.js
+++ js/main.js
@@ -9,35 +9,3 @@ if ('serviceWorker' in navigator) {
 } else {
   console.log('⚠️ Este navegador no soporta Service Workers.');
 }
-
-// --------------------- SCROLL SUAVIZADO (Menú) ---------------------
-$(document).ready(function () {
-  $("#menu a").click(function (e) {
-    e.preventDefault();
-
-    $("html, body").animate(
-      {
-        scrollTop: $($(this).attr("href")).offset().top,
-      },
-      800 // duración del efecto (milisegundos)
-    );
-
-    return false;
-  });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburgerBtn = document.getElementById('hamburger-btn');
-    const menu = document.getElementById('menu');
-
-    if (hamburgerBtn && menu) {
-        hamburgerBtn.addEventListener('click', () => {
-            // Esto agrega o quita la clase 'open'
-            menu.classList.toggle('open'); 
-            hamburgerBtn.classList.toggle('open');
-        });
-        
-        // ... (código para cerrar el menú al hacer clic en un enlace) ...
-    }
-    // ... (resto del código JS)
-});
